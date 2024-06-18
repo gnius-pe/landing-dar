4c78479f6406f37e4aea92646f655afc7f836fca
edilfonso2019 <benjaminedilfonso@gmail.com>
2024-06-18 16:29:59 -0500

    Agregue versionamiento de landing

--- js/script.js
+++ js/script.js
@@ -1,235 +1,238 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  ("use strict");
 
-	"use strict";
-	
-	/* =================================
+  /* =================================
 	LOADER 
 	=================================== */
-	$(".loader").delay(400).fadeOut();
-    $(".animationload").delay(400).fadeOut("fast");
+  $(".loader").delay(400).fadeOut();
+  $(".animationload").delay(400).fadeOut("fast");
 
-    var bgi = $("[data-background]");
-    bgi.length > 0 && bgi.each(function() {
-    	var e = $(this),
-    	t = e.attr('data-background');
-    	e.css({'background-image': 'url(' + t + ')'});
+  var bgi = $("[data-background]");
+  bgi.length > 0 &&
+    bgi.each(function () {
+      var e = $(this),
+        t = e.attr("data-background");
+      e.css({ "background-image": "url(" + t + ")" });
     });
 
-    var progressBar = $('.progress-bar');
-    progressBar.length > 0 && progressBar.each(function() {
-    	var e = $(this),
-    	t = e.attr('aria-valuenow');
-    	e.css({'width': t + '%'});
+  var progressBar = $(".progress-bar");
+  progressBar.length > 0 &&
+    progressBar.each(function () {
+      var e = $(this),
+        t = e.attr("aria-valuenow");
+      e.css({ width: t + "%" });
     });
 
-    /* =================================
+  /* =================================
 	SCROLL TO
 	=================================== */
-	$('a[href^="#"]').on('click', function(event) {
+  $('a[href^="#"]').on("click", function (event) {
+    var target = $(this.getAttribute("href"));
 
-	    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $("html, body").stop().animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        1000
+      );
+    }
+  });
 
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').stop().animate({
-	            scrollTop: target.offset().top
-	        }, 1000);
-	    }
-
-	});
-
-	
-	/* =================================
+  /* =================================
 	NAVBAR 
 	=================================== */
-	var top = jQuery(document).scrollTop();
-	var batas = 200;
-	var navbar = jQuery('.navbar-main');
-	var navbarnav = jQuery('.navbar-nav');
-	var header = jQuery('.header');
-	
-	
-	if ( top > batas ) {
-		navbar.addClass('stiky');
-		navbarnav.addClass('ml-auto');
-	}
-	jQuery(window).scroll(function () {
-		top = jQuery(document).scrollTop();
+  var top = jQuery(document).scrollTop();
+  var batas = 200;
+  var navbar = jQuery(".navbar-main");
+  var navbarnav = jQuery(".navbar-nav");
+  var header = jQuery(".header");
 
-		
-		if ( top > batas ) {
-			navbar.addClass('stiky');
-			navbarnav.addClass('ml-auto');
-		}else {
-			navbar.removeClass('stiky'); 
-			if(header.hasClass('header-1')){
-				navbarnav.removeClass('ml-auto');
-			}
-		}
-		
+  if (top > batas) {
+    navbar.addClass("stiky");
+    navbarnav.addClass("ml-auto");
+  }
+  jQuery(window).scroll(function () {
+    top = jQuery(document).scrollTop();
 
-	});
-	
-	/* =================================
+    if (top > batas) {
+      navbar.addClass("stiky");
+      navbarnav.addClass("ml-auto");
+    } else {
+      navbar.removeClass("stiky");
+      if (header.hasClass("header-1")) {
+        navbarnav.removeClass("ml-auto");
+      }
+    }
+  });
+
+  /* =================================
 	BANNER ROTATOR IMAGE 
 	=================================== */
-	var slides = $("#oc-fullslider"),
-        b = slides.find('.owl-slide');
-        b.each(function(){
-            var e = $(this),
-            ocImg = e.find('img').attr('src');
-            e.css({'background-image': 'url(' + ocImg + ')'});
+  var slides = $("#oc-fullslider"),
+    b = slides.find(".owl-slide");
+  b.each(function () {
+    var e = $(this),
+      ocImg = e.find("img").attr("src");
+    e.css({ "background-image": "url(" + ocImg + ")" });
 
-            console.log('tes ' + ocImg); 
-        });
+    console.log("tes " + ocImg);
+  });
 
+  slides.owlCarousel({
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    loop: true,
+    animateIn: "fadeIn",
+    animateOut: "rollOut",
+    pagination: false,
+    nav: true,
+    navText: [
+      "<span class='fa fa-chevron-left'></span>",
+      "<span class='fa fa-chevron-right'></span>",
+    ],
+    dots: false,
+  });
 
-    slides.owlCarousel({
-        items: 1,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true,
-        loop: true,
-        animateIn: 'fadeIn',
-        animateOut: 'rollOut',
-        pagination: false,
-        nav: true,
-		navText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
-		dots: false,
-    }); 
+  slides.on("translate.owl.carousel", function (e) {
+    $(".owl-item video").each(function () {
+      $(this).get(0).pause();
+    });
+  });
+  slides.on("translated.owl.carousel", function (e) {
+    $(".owl-item.active video").get(0).play();
+  });
 
-    slides.on('translate.owl.carousel',function(e){
-	    $('.owl-item video').each(function(){
-	      $(this).get(0).pause();
-	    });
-  	});
-  	slides.on('translated.owl.carousel',function(e){
-	    $('.owl-item.active video').get(0).play();
-  	})
-
-	/* =================================
+  /* =================================
 	BACK TO TOP 
 	=================================== */
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $(".cd-top");
 
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
-			$back_to_top.addClass('cd-fade-out');
-		}
-	});
+  //hide or show the "back to top" link
+  $(window).scroll(function () {
+    $(this).scrollTop() > offset
+      ? $back_to_top.addClass("cd-is-visible")
+      : $back_to_top.removeClass("cd-is-visible cd-fade-out");
+    if ($(this).scrollTop() > offset_opacity) {
+      $back_to_top.addClass("cd-fade-out");
+    }
+  });
 
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
-	
+  //smooth scroll to top
+  $back_to_top.on("click", function (event) {
+    event.preventDefault();
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      scroll_top_duration
+    );
+  });
 
-	/* =================================
+  /* =================================
 	OWL
 	=================================== */
-	
-	var caro = $("#caro");
-	caro.owlCarousel({
-		items: 1,
-		autoplay: true,
-		autoplayTimeout: 5000,
-		autoplayHoverPause: true,
-		loop: true,
-	});	
-	var caro2 = $("#caro-2");
-	caro2.owlCarousel({
-		autoplay: true,
-		margin: 30,
-		autoplayTimeout: 5000,
-		autoplayHoverPause: true,
-		items : 3,
-		dots: true,
-		loop: true,
-		responsive:{
-			0:{
-				items:1
-			},
-			768:{
-				items:2
-			},
-			1000:{
-				items:3
-			}
-		}
-	});
-	var testimony = $("#testimonial");
-	testimony.owlCarousel({
-		items: 1,
-		autoplay: true,
-		autoplayTimeout: 5000,
-		autoplayHoverPause: true,
-		loop: true,
-		animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-	});
-	
-	var testimony2 = $("#owl-testimony2");
-	testimony2.owlCarousel({
-		items: 1,
-		autoplay: true,
-		autoplayTimeout: 5000,
-		autoplayHoverPause: true,
-		navText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
-		dots: true,
-		loop: true
-	});
-	
-	
-	/* =================================
+
+  var caro = $("#caro");
+  caro.owlCarousel({
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    loop: true,
+  });
+  var caro2 = $("#caro-2");
+  caro2.owlCarousel({
+    autoplay: true,
+    margin: 30,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    items: 3,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+  var testimony = $("#testimonial");
+  testimony.owlCarousel({
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    loop: true,
+    animateIn: "fadeIn",
+    animateOut: "fadeOut",
+  });
+
+  var testimony2 = $("#owl-testimony2");
+  testimony2.owlCarousel({
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    navText: [
+      "<span class='fa fa-chevron-left'></span>",
+      "<span class='fa fa-chevron-right'></span>",
+    ],
+    dots: true,
+    loop: true,
+  });
+
+  /* =================================
 	MAGNIFIC POPUP
 	=================================== */
-	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-      disableOn: 700,
-      type: 'iframe',
-      mainClass: 'mfp-fade',
-      removalDelay: 160,
-      preloader: false,
+  $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
 
-      fixedContentPos: false
-    });
+    fixedContentPos: false,
+  });
 
-	$('.popup-gallery').magnificPopup({
-	  delegate: 'a',
-	  type: 'image',
-	  tLoading: 'Loading image #%curr%...',
-	  mainClass: 'mfp-img-mobile',
-	  gallery: {
-		enabled: true,
-		navigateByImgClick: true,
-		preload: [0,1]
-	  },
-	  image: {
-		tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-		titleSrc: function(item) {
-		  return item.el.attr('title') + '';
-		}
-	  }
-	});
+  $(".popup-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1],
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function (item) {
+        return item.el.attr("title") + "";
+      },
+    },
+  });
 
-	
-	
+  // Actualizar el año automáticamente
+  const currentYear = new Date().getFullYear();
+  $("#currentYear").text(currentYear);
+
+  // Asignar la versión de la aplicación
+  const appVersion = "0.0.1"; // Puedes cambiar esto según tu versión actual
+  $("#appVersion").text(appVersion);
 });
-
-
-
-
-  
-  
